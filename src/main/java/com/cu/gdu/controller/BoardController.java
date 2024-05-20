5eccf1affc65727e83004ed7df03ea5fad9e9949
JE0NGEUNJE0NG <dwej11@naver.com>
2024-05-20 10:34:49 +0900

    feat : 공지게시판 기능
    author : JE0NGEUNJE0NG
    
    공지게시판 기능 구현
    - 공지 리스트, 검색,  작성, 상세보기, 수정 및 삭제 기능

--- src/main/java/com/cu/gdu/controller/BoardController.java
+++ src/main/java/com/cu/gdu/controller/BoardController.java
@@ -247,15 +247,174 @@ public class BoardController {
 	
 	
 	// * 공지게시판------------------------------------------
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	// * 공지게시판 리스트 (페이징 조회 관련)-------------------------------------
+	@GetMapping("/noticeList.do")
+	public ModelAndView noticeList(@RequestParam(value="page" ,defaultValue="1") int currentPage
+									, ModelAndView mv ) {
+		String boardType = "N";
+		int listCount = boardService.selectBoardListCount(boardType);
+		PageInfoDto pi = pagingUtil.getPageInfoDto(listCount, currentPage, 5, 5);
+		List<BoardDto> list = boardService.selectBoardList(pi, boardType);
+		System.out.println(list);
+			
+		mv.addObject("pi", pi)
+		  .addObject("list", list)
+		  .setViewName("board/noticeList");
+			
+		return mv;
+	}
+		
+	// * 공지게시판 검색기능 (페이징 조회 관련)-------------------------------------
+	@GetMapping("/noticeSearch.do")
+	public ModelAndView noticeSearch(@RequestParam(value="page", defaultValue="1") int currentPage,
+									 @RequestParam Map<String, String> search, ModelAndView mv) {
+			
+		log.debug("search: {}", search);
+		search.put("boardType", "N");
+		int listCount = boardService.selectSearchListCount(search);
+		PageInfoDto pi = pagingUtil.getPageInfoDto(listCount,currentPage , 5, 5);
+		List<BoardDto> list = boardService.selectSearchList(search, pi);
+			
+		mv.addObject("pi", pi)
+		  .addObject("list", list)
+		  .addObject("search", search)
+		  .setViewName("board/noticeList");
+			  
+		return mv;
+	}
+		
+	// * 공지게시판 작성 관련 ------------------------------------------
+	@GetMapping("/noticeRegistForm.page")
+	public String noticeRegistForm() {
+		return "board/noticeRegistForm";
+	}
+		
+	@PostMapping("noticeRegist.do")
+	public String noticeRegist(BoardDto board, List<MultipartFile> uploadFiles
+							 , HttpSession session, RedirectAttributes redirectAttributes	) {
+		MemberDto loginUser = (MemberDto)session.getAttribute("loginUser");
+		board.setMemNo((loginUser.getMemNo()));
+		board.setBoardType("N");
+			
+		System.out.println(board);
+			
+		List<AttachDto> attachList = new ArrayList<>();	
+			
+		for(MultipartFile uploadFile: uploadFiles) {
+			if(uploadFile != null && !uploadFile.isEmpty()) {
+				//파일 업로드
+				Map<String, String> map = fileUtil.fileUpload(uploadFile, session, "board");
+					
+				// insert할 데이터 => AttachDto객체만들기 => attachList쌓기
+				attachList.add( AttachDto.builder()
+										 .filePath(map.get("filePath"))
+										 .filesystemName(map.get("filesystemName"))
+										 .originalName(map.get("originalName"))
+										 .refType("N")
+										 .build());			
+			}
+		}
+		board.setAttachList(attachList); // 첨부파일이 없었을 경우 텅빈리스트
+			
+		int result = boardService.insertBoard(board);
+			
+		// 성공시 => alert메세지와 함께 목록페이지로 이동
+		// 실패시 => alert메세지와 함께 작성페이지에 그대로
+			
+		if(attachList.isEmpty() && result == 1 || !attachList.isEmpty() && result == attachList.size() ) {
+			redirectAttributes.addFlashAttribute("alertMsg", "공지 작성에 성공하였습니다.");
+		}else {
+			redirectAttributes.addFlashAttribute("alertMsg", "공지 작성에 실패하였습니다.");
+			redirectAttributes.addFlashAttribute("historyBackYN", "Y");
+		}
+
+			return "redirect:/board/noticeList.do";
+		}
+		
+	// * 공지글 상세 관련 --------------------------------------------------
+	// /board/noticeIncrease.do?no=글번호
+	@GetMapping("/noticeIncrease.do")
+	public String noticeIncrease(int no) { // 조회수 증가용 (내글이 아닌 게시글 클릭시에만 호출)
+		// 조회수 증가
+		boardService.updateIncreaseCount(no);
+				
+		// 상세페이지 요청
+		return "redirect:/board/noticeDetail.do?no=" + no;
+	}
+			
+	// /board/detail.do?no=글번호
+	@GetMapping("/noticeDetail.do") // 게시글 상세 조회용 (내가 작성한 게시글 클릭시 곧바로 호출 | 수정완료 후 호출 곧바로 호출)
+	public String noticeDetail(int no, Model model) {
+		// 상세페이지에 필요한 데이터 담아서 포워딩
+		model.addAttribute("board", boardService.selectBoard(no));
+				
+		return "board/noticeDetail";
+	}
+		
+	// * 공지글 수정 및 삭제 관련 ------------------------------------------------------
+	@PostMapping("/noticeModifyForm.page")
+	public String noticeModifyForm(int no, Model model) {
+		model.addAttribute("board", boardService.selectBoard(no));
+		return "board/noticeModifyForm";
+	}	
+			
+	@PostMapping("/noticeModify.do")
+	public String noticeModify(BoardDto board, String[] delFileNo, List<MultipartFile> uploadFiles
+							 , HttpSession session, RedirectAttributes redirectAttributes) {
+				
+		// 삭제할 파일에 대한 정보를 조회해두기
+		List<AttachDto> delFileList = boardService.selectDelFileList(delFileNo);
+				
+		// 게시글 정보 수정 (update board)
+				
+		// 삭제할 첨부파일 => delete attachment + 파일자체를 삭제
+		// 추가할 첨부파일 => 파일들 업로드 + insert attachment
+		List<AttachDto> addFileList = new ArrayList<>();
+		for(MultipartFile uploadFile : uploadFiles) {
+			if(uploadFile != null && !uploadFile.isEmpty()) {
+				Map<String, String> map = fileUtil.fileUpload(uploadFile, session, "board");
+				addFileList.add( AttachDto.builder()
+										  .originalName(map.get("originalName"))
+										  .filePath(map.get("filePath"))
+										  .filesystemName(map.get("filesystemName"))
+										  .refType("N")
+										  .refNo(board.getBoardNo())
+										  .build()) ;
+			}
+		}
+				
+		board.setAttachList(addFileList);
+		// board : 글번호, 제목, 내용, 추가시킬첨부파일에대한정보(attachList)
+				
+		int result = boardService.updateBoard(board, delFileNo);
+			
+		if(result > 0) {
+			// 성공시 
+			// => 삭제할 첨부파일이 있었다면 => 해당 파일 찾아서 삭제되도록
+			for(AttachDto at : delFileList) {
+				new File(session.getServletContext().getRealPath(at.getFilePath() + "/" + at.getFilesystemName())).delete();
+			}
+		}else {
+			// 실패시
+			redirectAttributes.addFlashAttribute("alertMsg", "공지 수정 실패");
+			redirectAttributes.addFlashAttribute("historyBackYN", "Y");
+		}
+			
+		return "redirect:/board/noticeDetail.do?no=" + board.getBoardNo();
+	}
+			
+	@PostMapping("/noticeRemove.do")
+	public String deleteNotice(int no, RedirectAttributes redirectAttributes) {
+		int result = boardService.deleteBoard(no);
+				
+		if(result > 0) {
+			redirectAttributes.addFlashAttribute("alertMsg", "공지가 성공적으로 삭제되었습니다.");
+		}else {
+			redirectAttributes.addFlashAttribute("alertMsg", "공지 삭제 실패");
+		}
+		return "redirect:/board/noticeList.do";
+	}
+
 	// * 공지게시판------------------------------------------
 	
 	
