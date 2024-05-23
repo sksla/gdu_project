cd01610ea1b7de5c7389fd19093f5f06bb42ab43
hoyong <dnguq777@naver.com>
2024-05-23 15:17:09 +0900

    docs : VacationTypeDto 객체 추가
    docs : VacationTypeDto 객체 추가
    author : hoyong

---
+++ src/main/java/com/cu/gdu/dto/VacationTypeDto.java
@@ -0,0 +1,22 @@
+package com.cu.gdu.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@ToString
+@Builder
+public class VacationTypeDto {
+
+	int vacTypeNo;
+	String vacType;
+	String vacTypeDate;
+	
+}
