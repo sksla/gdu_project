7cf1e0071ad785a54df52734f4d863e0e6a39334
sksla <153481660+sksla@users.noreply.github.com>
2024-06-04 09:42:36 +0900

    update HomeController.java(로그인 유무에 따른 메인페이지 변경)
    author: 김영주
    
    로그인 유무에 따라 로그인 페이지, 메인페이지로 이동하게 하는 구문 추가

--- src/main/java/com/cu/gdu/controller/HomeController.java
+++ src/main/java/com/cu/gdu/controller/HomeController.java
@@ -1,15 +1,25 @@
 package com.cu.gdu.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.cu.gdu.dto.MemberDto;
 
 @Controller
 public class HomeController {
 
 	// 메인페이지
 	@GetMapping(value= {"/", "/main.page"})
-	public String mainPage() {
-		return "main";
+	public String mainPage(HttpSession session) {
+		MemberDto loginUser = (MemberDto)session.getAttribute("loginUser");
+		if(loginUser == null) {
+			return "main";
+			
+		}else{
+			return "member/mainpage";
+		}
 	}
 	
 	// 페이지 작성 예시 페이지
